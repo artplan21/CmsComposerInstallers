0de8b80a3394859f2c831cbcdd4ec551921fed2b
Stephan Jorek <stephan.jorek@gmail.com>
2014-07-27 01:36:11 +0200

    update

--- Classes/TYPO3/CMS/Composer/Installer/CoreInstaller.php
+++ Classes/TYPO3/CMS/Composer/Installer/CoreInstaller.php
@@ -223,7 +223,7 @@ class CoreInstaller implements \Composer\Installer\InstallerInterface {
 
 		$symlinks = array();
 		foreach($this->symlinks as $sourcePath => $targetPath) {
-			$symlinks[$sourcePrefix . $sourcePath] = $targetPrefix . $targetPath;
+			$symlinks[realpath($sourcePrefix . $sourcePath)] = $targetPrefix . $targetPath;
 		}
 		return $symlinks;
 	}
